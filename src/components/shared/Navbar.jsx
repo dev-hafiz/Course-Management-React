a9cfd94b991172562816c6157e01f550dc1c76d5
dev-hafiz <johnstarhafiz@gmail.com>
2024-09-03 17:07:52 +0600

    Install tailwind css and daisyui successfull and complete navbar comopnet and page setup successfully

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,68 @@
+/* eslint-disable react/no-unescaped-entities */
+import logo from "../../../public/logo.png";
+const Navbar = () => {
+  return (
+    <div className="px-3 md:px-28 mx-auto navbar bg-[#1C1E53] text-white">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-[#F47D9E] rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a>HOME</a>
+            </li>
+
+            <li>
+              <a>All Course</a>
+            </li>
+            <li>
+              <a>Student Dashboard</a>
+            </li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">
+          <img src={logo} className="w-[60px] h-full" alt="logo" />
+          Learner's
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Home</a>
+          </li>
+
+          <li>
+            <a>All Course</a>
+          </li>
+          <li>
+            <a>Student Dashboard</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <button className=" btn btn-outline text-white border-[#F47D9E] border-2 hover:border-white">
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
